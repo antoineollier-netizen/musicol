24a95e2d222f34ae4cd85b3a65dec1079085779f
aoacnantes <antoine.ollier@ac-nantes.fr>
2025-10-04 19:37:04 +0200

    Create /vue_coursInstruments.php

---
+++ vue_coursInstruments.php
@@ -0,0 +1,23 @@
+<HTML>
+
+<?php
+	include "vue/vue_entete.php";
+?>
+
+<section>
+
+<!-- affichage du titre de la page avec l'intitulé de l'instrument choisi
+	
+  <table>
+
+<!-- affichage de l’entête du tableau des cours liés à l'instrument
+	
+	
+<!-- affichage  de l’ensemble des  cours liés à l'instrument
+
+  </table>
+</section>
+		
+<p><a href="index.php">Retour Accueil</a></p>
+
+</HTML>

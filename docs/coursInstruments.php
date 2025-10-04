a849920880ac9b0a96bcb5e978ebc9c9dea0556f
aoacnantes <antoine.ollier@ac-nantes.fr>
2025-10-04 19:30:10 +0200

    Create coursInstruments.php

---
+++ docs/coursInstruments.php
@@ -0,0 +1,10 @@
+<?php
+// récupération de l'identifiant de l'intrument passé en paramètre
+
+// récupèration du nom de l'instrument via la fonction adéquate 
+
+// récupération des cours associés à l'identifiant grâce à la fontion get_lesCoursInstrument
+
+// affichage de  la  vue  vue_coursInstruments.php 
+
+?>

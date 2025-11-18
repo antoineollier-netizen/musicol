4755e7535216d7bb02d54f0c8c566261d62e1411
aoacnantes <antoine.ollier@ac-nantes.fr>
2025-11-18 18:42:22 +0100

    Delete docs/js directory

--- docs/js/script.js
+++
@@ -1,7 +0,0 @@
-// Sélectionne le bouton
-const btn = document.getElementById("darkModeBtn");
-
-// Écoute le clic pour basculer la classe 'dark' sur le body
-btn.addEventListener("click", function() {
-  document.body.classList.toggle("dark");
-});
